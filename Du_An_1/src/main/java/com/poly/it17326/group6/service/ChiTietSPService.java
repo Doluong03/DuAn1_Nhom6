2392c8f4a2e5bda4338bf682eb69dc9282fdcd31
DoLuong <luongddph27553@fpt.edu.vn>
2022-11-26 06:28:33 +0700

    update form sản phẩm

--- Du_An_1/src/main/java/com/poly/it17326/group6/service/ChiTietSPService.java
+++ Du_An_1/src/main/java/com/poly/it17326/group6/service/ChiTietSPService.java
@@ -4,7 +4,12 @@
  */
 package com.poly.it17326.group6.service;
 
+import com.poly.it17326.group6.domainmodel.Anh;
+import com.poly.it17326.group6.domainmodel.LoaiSP;
+import com.poly.it17326.group6.domainmodel.NSX;
+import com.poly.it17326.group6.domainmodel.SanPham;
 import com.poly.it17326.group6.response.ChiTietSpResponse;
+import com.poly.it17326.group6.response.ChiTietSpResponse_2;
 import java.util.ArrayList;
 
 /**
@@ -12,8 +17,26 @@ import java.util.ArrayList;
  * @author 123
  */
 public interface ChiTietSPService {
-    
+
     public ArrayList<ChiTietSpResponse> getAll();
-    
+
     public ArrayList<ChiTietSpResponse> getTimKiem(String ma);
+
+    public ArrayList<ChiTietSpResponse_2> getTimKiemLsp(String ten);
+
+    public ChiTietSpResponse_2 addSP(ChiTietSpResponse_2 ctsp);
+
+    public ChiTietSpResponse_2 updateSP(ChiTietSpResponse_2 ctsp);
+
+    public ArrayList<SanPham> getListSp();
+
+    public ArrayList<NSX> getListNsx();
+
+    public ArrayList<LoaiSP> getListLSp();
+
+    public ArrayList<Anh> getListA();
+
+    public ArrayList<ChiTietSpResponse_2> getAllFSP();
+
+    public ArrayList<ChiTietSpResponse_2> getTimKiemFSP(String ma);
 }
